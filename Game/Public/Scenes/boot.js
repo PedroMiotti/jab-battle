f3ed4e2334a01757f4a85cd3feffa1e591eb79fd
Pedro Miotti <pedromiotti7@gmail.com>
2020-04-07 16:38:04 -0300

    Loading Screen

--- Game/Public/Scenes/boot.js
+++ Game/Public/Scenes/boot.js
@@ -1,14 +1,80 @@
 class Boot extends Phaser.Scene {
     constructor(){
         super('boot')
-
-        
-
     }
 
     preload() {
 
-        // Tela Inicial 
+        // Escondendo a TextBox da Tela Nome
+        this.inputBox = document.getElementById('nameBox')
+        this.inputBox.style.display = "none"
+        
+        let progressBar = this.add.graphics();
+        let progressBox = this.add.graphics();
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(345, 355, 320, 50);
+            
+        let width = this.cameras.main.width;
+        let height = this.cameras.main.height;
+        
+        let loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 65,
+            text: 'Carregando...',
+            style: {
+                font: '20px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+        
+        let percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 5,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+        
+        let assetText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 50,
+            text: '',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        assetText.setOrigin(0.5, 0.5);
+        
+        this.load.on('progress', function (value) {
+            percentText.setText(parseInt(value * 100) + '%');
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(355, 365, 300 * value, 30);
+        });
+        
+        this.load.on('fileprogress', function (file) {
+            assetText.setText('Carregando arquivo : ' + file.key);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+            assetText.destroy();
+        });
+        
+        this.load.image('logo', 'Assets/UI/Logo.png');
+        
+        for (let i = 0; i < 800; i++) {
+            this.load.image('logo'+i, 'Assets/UI/Logo.png');
+        }
+            // Tela Inicial 
 
             // Background
             this.load.image('telainicialBG', 'Assets/img/TelaInicial.png')
@@ -16,8 +82,6 @@ class Boot extends Phaser.Scene {
             this.load.image('botaoJogar', 'Assets/UI/BotaoJogar.png')
             // Botao Config
             this.load.image('configBotao', 'Assets/UI/ConfigBotao1.png')
-
-// ------------------------------------------------------------------------
 
          // Tela Nome 
 
@@ -27,22 +91,7 @@ class Boot extends Phaser.Scene {
             this.load.image('botaoVoltar', 'Assets/UI/BackButton.png')
             // Botao Fight
             this.load.image('botaoFight', 'Assets/UI/FightButton.png' )
-
-// ------------------------------------------------------------------------
-
-    // Tela Encontrando Jogador
-
-        // Background 
-        this.load.image('telaencontrarjogadorBG', ' Assets/img/TelaEncontrandoJogador.png')
-        // Ponto 
-        // this.load.spritesheet('pontoanimacao', 'Assets/UI/pontosSpritesheet.png', {frameWidth:6 , frameHeight: 7})
-        this.load.image('ponto', 'Assets/UI/ponto.png')
-
-        // Luva Azul
-        this.load.image('luvaazul', 'Assets/UI/LuvaAzul.png')
-
-// ------------------------------------------------------------------------
-        
+            
         // Tela Ringue
 
             // Background 
@@ -51,16 +100,26 @@ class Boot extends Phaser.Scene {
             // Sprites
                 // Tommy
                 this.load.atlas("Tommy", "Assets/Sprites/Spritesheet/sprites.png", 'Assets/Sprites/JSON/sprites.json')
+        
+
+        
  
     }
 
     create() {
 
+        this.add.image(512, 384, 'logo');
+
         // Escondendo a TextBox da Tela Nome
         this.inputBox = document.getElementById('nameBox')
         this.inputBox.style.display = "none"
 
-        this.scene.start('telaInicial')
+        setTimeout(() => { 
+            this.scene.start('telaInicial') 
+        }, 100);
+        
+
+        
 
     }
 
