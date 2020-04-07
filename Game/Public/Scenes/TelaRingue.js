f25d111bbc25c93b38e7ab33f2d0b378ec42a6b6
Pedro Miotti <pedromiotti7@gmail.com>
2020-04-07 16:32:20 -0300

    Modules

--- Game/Public/Scenes/TelaRingue.js
+++ Game/Public/Scenes/TelaRingue.js
@@ -2,83 +2,26 @@ class TelaRingue extends Phaser.Scene {
     constructor(){
         super('telaRingue')
     }
-
     
+
     create(){
 
         // Setting Background
         this.background = this.add.image(0, 0, 'telaringueBG')
         this.background.setOrigin(0, 0)
 
-        // // Loading Characters 
-        this.tommy = this.physics.add.sprite(300, 450,'Tommy', 'Tommy_01.png')
-
-        this.tommy.setCollideWorldBounds(true);
-
-        // Adding physics to the character
-        this.physics.add.collider(this.tommy) 
-        this.physics.world.enable(this.tommy)  
-        
-
-
-    // ANIMATIONS
-        // TOMMY
-            //WALK
-            this.anims.create({
-                key: "walk",
-                repeat: -1,
-                frameRate: 6,
-                frames: this.anims.generateFrameNames('Tommy', {
-                    prefix: 'Tommy_',
-                    suffix: ".png",
-                    start: 7,
-                    end: 10,
-                    zeroPad: 2,
-                })
-            })
-
-            // IDLE
-            this.anims.create({
-                key: "idle",
-                repeat: -1,
-                frameRate: 3,
-                frames: this.anims.generateFrameNames('Tommy', {
-                    prefix: 'Tommy_',
-                    suffix: ".png",
-                    start: 3,
-                    end: 4,
-                    zeroPad: 2,
-                })
-            })
-
-            //JAB
-            this.anims.create({
-                key: "jab",
-                repeat: -1,
-                frameRate: 4,
-                frames: this.anims.generateFrameNames('Tommy', {
-                    prefix: 'Tommy_',
-                    suffix: ".png",
-                    start: 5,
-                    end: 6,
-                    zeroPad: 2,
-                })
-            })
-
-            // DIRETO
-            this.anims.create({
-                key: "direto",
-                repeat: -1,
-                frameRate: 4,
-                frames: this.anims.generateFrameNames('Tommy', {
-                    prefix: 'Tommy_',
-                    suffix: ".png",
-                    start: 1,
-                    end: 2,
-                    zeroPad: 2,
-                })
-            })
-
+        // Adding Characters
+        this.chosenChar = 'tommy' // Pegar da tela ChooseChar
+        this.character
+        if(this.chosenChar == 'tommy'){
+            this.character = this.physics.add.existing(new Tommy(this, 300, 450))
+        }
+       
+       // Adding Physics to the Character
+       this.character.setCollideWorldBounds(true); 
+       this.physics.add.collider(this.character) 
+       this.physics.world.enable(this.character) 
+       
 
         // ADDING KEYS
         this.keys = this.input.keyboard.addKeys({
@@ -91,8 +34,6 @@ class TelaRingue extends Phaser.Scene {
         // Escondendo a TextBox da Tela Nome
         this.inputBox = document.getElementById('nameBox')
         this.inputBox.style.display = "none"
-
-        
 
     }
 
@@ -117,33 +58,38 @@ class TelaRingue extends Phaser.Scene {
         movePlayers(){
    
             if (this.keys.left.isDown){
-
-                this.tommy.anims.play('walk', true);
-                this.tommy.setVelocityX(-160);
+                // debugger
+                this.character.anims.play('walk', true)
+                this.character.setVelocityX(-160)
             }
 
-            else if (this.keys.right.isDown){
+            else if(this.keys.right.isDown){
 
-                this.tommy.setVelocityX(160);
-                this.tommy.anims.play('walk', true);
-
-            }
-
-            else if (this.keys.jab.isDown){
-                this.tommy.anims.play('jab')
-            }
-
-            else if (this.keys.direto.isDown){
-                this.tommy.anims.play('direto');
+                this.character.setVelocityX(160)
+                this.character.anims.play('walk', true)
 
             }
 
             else{
-
-                this.tommy.setVelocityX(0);
-                this.tommy.anims.play('idle');
+                this.character.setVelocityX(0)
+                this.character.anims.play('idle');
 
             }
+
+            
+                
+            if(this.keys.jab.isDown){
+                this.character.anims.play('jab')
+                
+            }
+            else if(this.keys.direto.isDown){
+            
+                this.character.anims.play('direto');
+
+            }
+            
+
+            
 
         }
 }
