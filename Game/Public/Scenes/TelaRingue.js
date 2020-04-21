2ef9526423543299dc920778f9619c0fd243cc56
Pedro Miotti <pedromiotti7@gmail.com>
2020-04-21 12:36:43 -0300

    Able to add the player on the create() function | Whent put in the initPlayers() function it doesn't work

--- Game/Public/Scenes/TelaRingue.js
+++ Game/Public/Scenes/TelaRingue.js
@@ -5,13 +5,14 @@ class TelaRingue extends Phaser.Scene {
      
 
     create(){
+        this.self = this
         
         // Setting Background
         this.background = this.add.image(0, 0, 'telaringueBG')
         this.background.setOrigin(0, 0)
 
-        this.initPlayers()
-        
+        // this.initPlayers()
+        this.player = new Player(this, '1234', 275, 630, 'Tommy', 100)        
 
     }
 
@@ -21,32 +22,42 @@ class TelaRingue extends Phaser.Scene {
     }
 
     initPlayers(){
-        this.self = this
-        this.players = []
+        
+        
+        this.players = new Array()
 
         socket.emit('PLAYERS_DATA')
 
         // ID's in room
-        socket.on('PlayerInRoom', (data) => {
+        socket.on('PlayerInRoom', (dataID) => {
+            
             // Both players [ID's]
-            this.players = data
+            this.players = [
+                dataID[0],
+                dataID[1]
+            ]
+
         })
         
-
         // Inicializando Jogadores
-        // Data = players.info
+        //? Data = players.info
         socket.on('PlayerData', (data) => {
-            for(let i in this.players){
-                if(data.id == this.players[i]){
-                    player = new Player(this.self, data.id, data.x, data.y, data.character, data.life)  
+            
+            this.players.forEach(i => {
+                
+                if(i === data.id){
+                    
+                    this.self.player = new Player(this.self, data.id, data.x, data.y, data.character, data.life)  
+                    debugger
                 }
 
                 else {
+                    debugger
+                    return
                     // oponent = new Oponent(data.args)
-                    this.oponent = true
-                    
+                    // this.self.player = new Player(this.self, data.id, data.x, data.y, data.character, data.life)
                 }
-            }
+            })
             
 
         })
